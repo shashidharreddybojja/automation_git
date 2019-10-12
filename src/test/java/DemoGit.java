7dc68931bbfea49e57a2b08cbf21a0e227e9c7b9
Chanukyakatepalli <45603551+Chanukyakatepalli@users.noreply.github.com>
2019-10-12 19:38:10 +0100

    modified DemoGit

--- src/test/java/DemoGit.java
+++ src/test/java/DemoGit.java
@@ -2,6 +2,9 @@ public class DemoGit {
 
     public static void main(String[] args) {
         System.out.println("Git demo");
+
+        System.out.println("Chanukya commited changes");
+        System.out.println("Chanukya commited changes");
     }
 
 }
