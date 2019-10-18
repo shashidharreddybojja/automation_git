618ef2a2ab4b3e27cc1e031324ec96f0db0838e8
shashidharreddybojja <56484976+shashidharreddybojja@users.noreply.github.com>
2019-10-18 16:49:28 +0100

    commit

--- src/test/java/DemoGit.java
+++ src/test/java/DemoGit.java
@@ -3,8 +3,8 @@ public class DemoGit {
     public static void main(String[] args) {
         System.out.println("Git demo");
 
-        System.out.println("Chanukya commited changes");
-        System.out.println("Chanukya commited changes");
+        System.out.println("Chanukyacommitedchanges");
+        System.out.println("Chanukyacommitedchanges");
 
     }
 
